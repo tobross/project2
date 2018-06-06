cead6ffdce4041271349af08d215f4bae84d676f
Michael Dillie <michaeldillie@gmail.com>
2018-06-06 09:20:34 -0500

    Can now sign up

---
+++ models/event.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+  var Event = sequelize.define("Event", {
+    eventName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
+
+  Event.associate = function(models) {
+
+    Event.belongsTo(models.User, {
+      // foreignKey: {
+      //   allowNull: false
+      // }
+    });
+  }
+  return Event;
+};
---
+++ routes/event-api-route.js
@@ -0,0 +1,39 @@
+var db = require("../models");
+var passport = require("../config/passport");
+
+module.exports = function(app) {
+
+  // {
+  //   "eventName": "Fun Time",
+  //   "date": "2-2-20",
+  //   "location": "1234 Hello Ln, Austin TX, 78730",
+  //   "description": "Come have fun!"
+  // }
+
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json("/api/events");
+  });
+
+  app.get("/api/events", function(req, res) {
+    db.Event.findAll({}).then(function(result) {
+      res.json(result);
+    });
+  });
+
+
+  app.post("/api/events", function(req, res) {
+    db.Event.create({
+      // Look into a pointer in sequelize
+      // UserId: current user id
+      eventName: req.body.eventName,
+      date: req.body.date,
+      location: req.body.location,
+      description: req.body.description
+    }).then(function(result){
+      res.json(result);
+    }).catch(function(err) {
+      console.log("ERROR " + err);
+      res.json(err);
+    })
+  });
+}
