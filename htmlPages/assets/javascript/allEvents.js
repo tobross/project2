b42d44619e21d08f64db3e60fb82bd72f88afae6
ektravel <38017980+ektravel@users.noreply.github.com>
2018-06-05 22:31:46 -0500

    added all-events.html

---
+++ htmlPages/assets/javascript/allEvents.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    var num_searches = 0;
+    var nextPage = 1;
+
+    $("#nextEvents").on("click", function (event) {
+        event.preventDefault();
+        nextPage ++;
+
+        loadResults();  
+    }); 
+
+    $("#previousEvents").on("click", function (event) {
+        event.preventDefault();
+        nextPage --;
+
+        loadResults();  
+    }); 
+
+    function loadResults(){
+        $("#recipeCard").empty();
+
+        var recipeSearchInput = $("#dish-input").val();
+
+        if ($("#ingredient-input").val())
+            recipeSearchInput += "%20" + $("#ingredient-input").val();
+
+        if ($("#cuisine-input").val())
+            recipeSearchInput += "%20" + $("#cuisine-input").val();
+        
+        var recipeObject = {
+            input1: $("#dish-input").val(),
+            input2: $("#ingredient-input").val(),
+            input3: $("#cuisine-input").val()
+        }
+        firebase.database().ref().push({
+             recipeObject
+        })
+    };
+
+});
