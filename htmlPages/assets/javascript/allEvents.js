c79da7a57d07d632490c6711888a147c551425f3
ektravel <38017980+ektravel@users.noreply.github.com>
2018-06-06 11:03:55 -0500

    added userLogin.html

--- htmlPages/assets/javascript/allEvents.js
+++ htmlPages/assets/javascript/allEvents.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    var num_searches = 0;
     var nextPage = 1;
 
     $("#nextEvents").on("click", function (event) {
@@ -17,24 +16,11 @@ $(document).ready(function () {
     }); 
 
     function loadResults(){
-        $("#recipeCard").empty();
+        $("#eventsCard").empty();
 
-        var recipeSearchInput = $("#dish-input").val();
+        var eventCategoryInput = $("#category").val();
 
-        if ($("#ingredient-input").val())
-            recipeSearchInput += "%20" + $("#ingredient-input").val();
-
-        if ($("#cuisine-input").val())
-            recipeSearchInput += "%20" + $("#cuisine-input").val();
-        
-        var recipeObject = {
-            input1: $("#dish-input").val(),
-            input2: $("#ingredient-input").val(),
-            input3: $("#cuisine-input").val()
-        }
-        firebase.database().ref().push({
-             recipeObject
-        })
+        if ($("#category").val())
+        eventCategoryInput += $("#category").val();
     };
-
 });
