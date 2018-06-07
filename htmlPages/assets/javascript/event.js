d3357c87c6ad310a6ed140be54808182e5bec87b
Tobey Ross <tobross@gmail.com>
2018-06-07 12:44:46 -0500

    stuff

--- htmlPages/assets/javascript/event.js
+++ htmlPages/assets/javascript/event.js
@@ -20,24 +20,14 @@ $(document).ready(function () {
 
     $("#navigation").on("change", function() {
         console.log("redirect func running");
-        if (this.value === "events") {
-            if (userID) {
-                window.location = "event.html"
-            } else {
-                window.location = "login.html"
-            }
-        } else if (this.value === "vendors") {
-            if (userID) {
-                window.location = "vendor.html"
-            } else {
-                window.location = "login.html"
-            }
-        } else if (this.value === "home") {
-            if (userID) {
-                window.location = "landing.html"
-            } else {
-                window.location = "login.html"
-            }
-        }
+      if (this.value === "events") {
+          window.location = "event.html"
+      }
+     else if (this.value === "vendors") {
+         window.location = "vendor.html"
+     }
+     else {
+         window.location = "landing.html"
+     }
     });
 });
