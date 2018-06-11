b637a14ce29a35ac465806713404993fad64661f
Michael Dillie <michaeldillie@gmail.com>
2018-06-10 21:31:12 -0500

    Fixed up the clickable boxes on landing page to route

--- htmlPages/assets/javascript/landing.js
+++ htmlPages/assets/javascript/landing.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
             console.log("result from api/rpute-getter/event");
         });
     });
-    // $("#eventDiv").on("click", function () {
-    //     console.log("eventDivCLicked");
-    //     $.get("/api/route-getter/event", function (result) {
-    //         console.log("result from api/rpute-getter/event");
-    //         console.log(result);
-    //         console.log("result from api/rpute-getter/event");
-    //     });
-    // });
+    $("#eventBox").on("click", function () {
+        console.log("eventDivCLicked");
+        $.get("/api/route-getter/event", function (result) {
+            console.log("result from api/rpute-getter/event");
+            console.log(result);
+            console.log("result from api/rpute-getter/event");
+        });
+    });
     $("#BusinessButton").on("click", function () {
         console.log("businessButtonClicked");
         $.get("/api/route-getter/vendor", function (result) {
@@ -23,14 +23,14 @@ $(document).ready(function () {
             console.log("result from api/rpute-getter/vendor");
         });
     });
-    // $("#vendorDiv").on("click", function () {
-    //     console.log("vendorDivClicked");
-    //     $.get("/api/route-getter/vendor", function (result) {
-    //         console.log("result from api/rpute-getter/vendor");
-    //         console.log(result);
-    //         console.log("result from api/rpute-getter/vendor");
-    //     });
-    // });
+    $("#vendorBox").on("click", function () {
+        console.log("vendorDivClicked");
+        $.get("/api/route-getter/vendor", function (result) {
+            console.log("result from api/rpute-getter/vendor");
+            console.log(result);
+            console.log("result from api/rpute-getter/vendor");
+        });
+    });
     $("#eventUrl").on("click", function () {
         console.log("navAllEventsButtonClicked");
         $.get("/api/route-getter/all-events", function (result) {
